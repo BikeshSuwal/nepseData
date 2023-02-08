866d7cd82fe530ea6425bd862365701c76718584
BikeshSuwal <bikeshsuwal776@gmail.com>
2023-02-08 20:10:26 +0545

    error handling added and tqdm used for loading visual

--- nepsedata.py
+++ nepsedata.py
@@ -1,43 +1,54 @@
 import requests
-from bs4 import BeautifulSoup
-import pandas as pd
 import re
+import pandas as pd
+from bs4 import BeautifulSoup
 from companylist import companyArray, sectorName
+import tqdm
 
+# Initialize a dictionary to store the data
 data = {'Company': [], 'LTP': [], 'EPS': [], 'P/E ratio': []}
+
+# Create a DataFrame from the dictionary
 df = pd.DataFrame(data)
 
-for companyName in companyArray:
-    url = "https://merolagani.com/CompanyDetail.aspx?symbol="+companyName
+for companyName in tqdm.tqdm(companyArray):
+    try:
+        # Form the URL for each company
+        url = "https://merolagani.com/CompanyDetail.aspx?symbol=" + companyName
 
-    r = requests.get(url)
-    htmlContent = r.content
+        # Send a GET request to the URL
+        r = requests.get(url)
+        r.raise_for_status()
 
-    soup = BeautifulSoup(htmlContent, 'html.parser')
+        # Extract the content of the response
+        html_content = r.content
 
-    company = companyName
+        # Create a BeautifulSoup object from the content
+        soup = BeautifulSoup(html_content, 'html.parser')
 
-    datatext = soup.find_all("td")
+        # Get the company name
+        company = companyName
 
-    ltp = datatext[2].text.strip()
-    eps = datatext[9].text.strip()
-    pe = datatext[10].text.strip()
+        # Find all `td` elements in the content
+        data_text = soup.find_all("td")
 
-    # to reomve \r\n    in between eps value and fiscal year
-    eps = re.sub(
-        r'\r\n                                ', '', eps)
+        # Extract the values for LTP, EPS, and P/E ratio
+        ltp = data_text[2].text.strip()
+        eps = data_text[9].text.strip()
+        pe = data_text[10].text.strip()
 
-    # print(df)
-    # df['Company'] = [title]
-    # df['LPT'] = [ltp]
-    # df['EPS'] = [eps]
-    # df['P/E ratio'] = [pe]
-    # df = df.append({'Company': title, 'LPT': ltp, 'EPS': eps,
-    #                'P/E ratio': pe}, ignore_index=True)
-    # print(len(df))
-    df.loc[len(df)] = [company, ltp, eps, pe]
+        # Remove the unwanted characters from the EPS value
+        eps = re.sub(r'\r\n                                ', '', eps)
 
-    df.to_json(sectorName+'.json')
+        # Append the values to the DataFrame
+        df = df.append({'Company': company, 'LTP': ltp,
+                       'EPS': eps, 'P/E ratio': pe}, ignore_index=True)
+    except Exception as e:
+        print(f"Error while processing company {companyName}: {e}")
+
+# Save the DataFrame as a JSON file
+df.to_json(sectorName + '.json', orient='records')
+
 print(df)
 input("This data has been stored in "+sectorName +
       ".json file in current directory.\n Press any key and enter to exit.")
